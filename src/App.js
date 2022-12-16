64b302fe2493e1cf806cb6409e8491f6285f6dfb
UliaKarpova <chelovek-e-95@mail.ru>
2022-12-16 22:39:30 +0300

    test auth

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import './App.css';
+import FormWithValidation from './components/FormWithValidation';
+import { CurrentUserContext } from './contexts/CurrentUserContext';
+import { auth, getUserInfo, getUsers } from './utils/AuthApi';
+
+
+function App() {
+  const [currentUser, setCurrentUser] = useState({});
+  const [allUsers, setAllUsers] = useState([]);
+
+  const { values, handleChange, errors, isValid, resetForm } = FormWithValidation();
+  console.log(currentUser);
+  console.log(allUsers);
+
+
+  function onChange(event) {
+    handleChange(event);
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+    auth({
+      phone: values.phone,
+      password: values.password,
+      refreshToken: '',
+      grantType: 'PASSWORD'
+    }).then((res) => {
+      console.log(res);
+    }).then((res) => {
+      Promise.all([
+        getUserInfo(),
+        getUsers()
+      ])
+      .then(([data, arr]) => {
+        setCurrentUser({
+          firstName: data.firstName,
+          id: data.id,
+          patronymic: data.patronymic,
+          phone: data.phone,
+          role: data.role,
+          surname: data.surname
+        });
+        setAllUsers(arr);
+      });
+    }).catch((err) => console.log(err));
+    resetForm();
+  }
+
+  return (
+    <div className="App">
+      <form className='form' onSubmit={onSubmit}>
+        <h2 className='form__title'>Авторизация</h2>
+        <label htmlFor='phone' className='form__label'>Телефон</label>
+        <input id='phone' onChange={onChange} name='phone' className='form__phone' type='tel' />
+        <label htmlFor='password' className='form__label'>Пароль</label>
+        <input id='password' onChange={onChange} name='password' className='form__password' type='password' />
+        <button type='submit' className='form__button'>Отправить</button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/utils/AuthApi.js
@@ -0,0 +1,72 @@
+const BASE_URL = 'http://localhost:8080';
+const jwt = localStorage.getItem('token');
+
+const getResponseData = (res) => {
+    console.log(res);
+    if (res.ok) {
+        return res.json();
+    } else {
+        return res.json()
+            .then((data) => {
+                return Promise.reject(data.validation?.body.message || data.message);
+            })
+    }
+}
+
+/*export const register = (data) => {
+    return fetch(`${BASE_URL}/signup`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    }).then(getResponseData)
+    .catch((err) => {
+        return typeof err !== 'string' ? Promise.reject(connectionError) : Promise.reject(err);
+    })
+}*/
+
+export const auth = (data) => {
+    console.log(data);
+    return fetch(`${BASE_URL}/auth/sign-in`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+        }).then(getResponseData)
+        .then((data) => {
+            if (data.accessToken) {
+                localStorage.setItem('token', data.accessToken);
+                return data;
+            }
+        }).catch((err) => {
+            return typeof err !== 'string' ? Promise.reject("Ошибка подключения") : Promise.reject(err);
+        })
+
+}
+
+export const getUserInfo = () => {
+    return fetch(`${BASE_URL}/auth/me`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${jwt}`,
+          },
+    }).then(getResponseData)
+    .catch((err) => {
+        return typeof err !== 'string' ? Promise.reject('Ошибка подключения') : Promise.reject(err);
+    })}
+
+
+    export const getUsers = () => {
+        return fetch(`${BASE_URL}/admin/users`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${jwt}`,
+              },
+        }).then(getResponseData)
+        .catch((err) => {
+            return typeof err !== 'string' ? Promise.reject('Ошибка подключения') : Promise.reject(err);
+        })}
