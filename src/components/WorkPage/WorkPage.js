c516c3f94d57117490cf1a4daca26e8643d8d1ad
Olga Akhlamova <olga-akhlamova@yandex.ru>
2022-12-17 20:55:23 +0300

    add new components

--- src/components/WorkPage/WorkPage.js
+++ src/components/WorkPage/WorkPage.js
@@ -1,9 +1,20 @@
 import React from "react";
+import { NavLink } from 'react-router-dom';
 import ".WorkPage.css";
 
 function WorkPage() {
   return (
-    <div className="work-page"></div>
+    <div className="work__page">
+      <NavLink to="#" >
+        <button type='button' className='w-page__button'>Загрузить данные</button>
+      </NavLink>
+      <NavLink to="#" >
+        <button type='button' className='w-page__button'>Предварительный просмотр</button>
+      </NavLink>
+      <NavLink to="#" >
+        <button type='button' className='w-page__button'>Скачать отчет</button>
+      </NavLink>
+    </div>
   );
 }
 export default WorkPage
